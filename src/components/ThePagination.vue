43d6f1a8fce1aff20d23473ef65f7344cb383a17
NguyenDonUET <nguyendoncb@gmail.com>
2023-10-31 11:06:35 +0700

    responsive: the pagination

--- src/components/ThePagination.vue
+++ src/components/ThePagination.vue
@@ -4,11 +4,11 @@
       v-if="totalResult >= 1"
       :total-items="totalResult"
       :items-per-page="itemsPerPage"
-      :max-pages-shown="5"
+      :max-pages-shown="4"
       v-model="page"
       :on-click="onClickHandler"
       :hide-prev-next-when-ends="false"
-      :show-ending-buttons="true"
+      :show-ending-buttons="false"
     >
       <template #first-page-button>
         <div class="flex items-center justify-center">
@@ -127,29 +127,4 @@ const onClickHandler = (page) => {
 // });
 </script>
 
-<style>
-.pagination-container {
-  display: flex;
-  column-gap: 10px;
-}
-.paginate-buttons {
-  height: 40px;
-  width: 40px;
-  border-radius: 20px;
-  cursor: pointer;
-  background-color: rgb(242, 242, 242);
-  border: 1px solid rgb(217, 217, 217);
-  color: black;
-}
-.paginate-buttons:hover {
-  background-color: #d8d8d8;
-}
-.active-page {
-  background-color: #3498db;
-  border: 1px solid #3498db;
-  color: white;
-}
-.active-page:hover {
-  background-color: #2988c8;
-}
-</style>
+<style></style>
