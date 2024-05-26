626440cbb4c67d83279b6611eb733b4bea8b2522
NguyenDonUET <nguyendoncb@gmail.com>
2024-05-26 09:47:35 +0700

    feat: handle username text overflow

--- src/components/User/UserGeneralInfo.vue
+++ src/components/User/UserGeneralInfo.vue
@@ -2,11 +2,11 @@
   <div
     class="general col-start-2 col-span-3 pl-6 lg:pl-0 space-y-1 md:space-y-0 lg:col-start-2 lg:col-end-4"
   >
-    <h2 class="text-xl md:text-2xl font-bold">{{ name }}</h2>
+    <h2 class="text-xl md:text-2xl font-bold truncate">{{ name }}</h2>
     <p class="text-base text-lightBlue">@{{ name }}</p>
   </div>
 </template>
 
 <script setup>
-defineProps(["name"]);
+defineProps(["name"])
 </script>
